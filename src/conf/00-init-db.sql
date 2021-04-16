1e220d9ead8f56b31cffa6e62a6e90fadfe0fee1
Marco Espinosa <hi@marcocespinosa.es>
2021-04-16 17:49:55 +0200

    Testing database

--- src/conf/00-init-db.sql
+++ src/conf/00-init-db.sql
@@ -1,10 +1,8 @@
-CREATE TABLE IF NOT EXISTS temp (
-id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-field1 VARCHAR(30) NOT NULL
-); 
+CREATE TABLE IF NOT EXISTS temp 
+(
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    field1 VARCHAR(30) NOT NULL
+)
+;
 
-INSERT INTO temp(field1)
-VALUES
-	('My first task'),
-	('It is the second task'),
-	('This is the third task of the week');
+INSERT INTO temp (field1) VALUES ('My first task');
