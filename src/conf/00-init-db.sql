281ce3c12c41d2f4d73125c19178e11b9c9aba31
Marco Espinosa <hi@marcocespinosa.es>
2021-04-18 22:29:18 +0200

    Reordering tables creation

--- src/conf/00-init-db.sql
+++ src/conf/00-init-db.sql
@@ -4,6 +4,14 @@ SET foreign_key_checks = 0;
 
 SET NAMES utf8mb4;
 
+
+CREATE TABLE Artist (
+  id int unsigned NOT NULL AUTO_INCREMENT,
+  name varchar(200) NOT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
 CREATE TABLE Album (
   id int unsigned NOT NULL AUTO_INCREMENT,
   name varchar(100) NOT NULL,
@@ -18,28 +26,10 @@ CREATE TABLE Album (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
-CREATE TABLE Artist (
-  id int unsigned NOT NULL AUTO_INCREMENT,
-  name varchar(200) NOT NULL,
-  PRIMARY KEY (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-
 CREATE TABLE Files (
   id int unsigned NOT NULL AUTO_INCREMENT,
   path varchar(512) NOT NULL,
   PRIMARY KEY (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-
-CREATE TABLE Playlist (
-  id int unsigned NOT NULL AUTO_INCREMENT,
-  name varchar(100) NOT NULL,
-  genre varchar(100) DEFAULT NULL,
-  song_id int unsigned NOT NULL,
-  PRIMARY KEY (id),
-  KEY song_id (song_id),
-  CONSTRAINT Playlist_ibfk_1 FOREIGN KEY (song_id) REFERENCES Songs (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
@@ -58,6 +48,17 @@ CREATE TABLE Songs (
   CONSTRAINT Songs_ibfk_1 FOREIGN KEY (album_id) REFERENCES Album (id),
   CONSTRAINT Songs_ibfk_2 FOREIGN KEY (artist_id) REFERENCES Artist (id),
   CONSTRAINT Songs_ibfk_3 FOREIGN KEY (file_id) REFERENCES Files (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+CREATE TABLE Playlist (
+  id int unsigned NOT NULL AUTO_INCREMENT,
+  name varchar(100) NOT NULL,
+  genre varchar(100) DEFAULT NULL,
+  song_id int unsigned NOT NULL,
+  PRIMARY KEY (id),
+  KEY song_id (song_id),
+  CONSTRAINT Playlist_ibfk_1 FOREIGN KEY (song_id) REFERENCES Songs (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
