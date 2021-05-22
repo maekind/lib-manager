ef6aa977953834f278b0837b48684d056ad56f01
Marco Espinosa <hi@marcocespinosa.es>
2021-05-22 23:29:57 +0200

    Pylint corrections

--- src/lib/database/tables.py
+++ src/lib/database/tables.py
@@ -50,7 +50,8 @@ TABLES_CREATE['configuration'] = (
     "  `spotify_client_secret` varchar(128),"
     "  PRIMARY KEY (`id`),"
     "  KEY `user_id` (`user_id`),"
-    "  CONSTRAINT `configuration_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `login` (`id`) ON DELETE CASCADE"
+    "  CONSTRAINT `configuration_ibfk_1` FOREIGN KEY (`user_id`) "
+    "  REFERENCES `login` (`id`) ON DELETE CASCADE"
     ") ENGINE=InnoDB")
 
 TABLES_CREATE['artist'] = (
@@ -74,7 +75,8 @@ TABLES_CREATE['album'] = (
     "  `artist_id` int unsigned NOT NULL,"
     "  PRIMARY KEY (`id`),"
     "  KEY `artist_id` (`artist_id`),"
-    "  CONSTRAINT `album_ibfk_1` FOREIGN KEY (`artist_id`) REFERENCES `artist` (`id`) ON DELETE CASCADE"
+    "  CONSTRAINT `album_ibfk_1` FOREIGN KEY (`artist_id`) "
+    "  REFERENCES `artist` (`id`) ON DELETE CASCADE"
     ") ENGINE=InnoDB")
 
 
@@ -99,9 +101,12 @@ TABLES_CREATE['songs'] = (
     "  KEY `album_id` (`album_id`),"
     "  KEY `artist_id` (`artist_id`),"
     "  KEY `file_id` (`file_id`),"
-    "  CONSTRAINT `songs_ibfk_1` FOREIGN KEY (`album_id`) REFERENCES `album` (`id`) ON DELETE CASCADE,"
-    "  CONSTRAINT `songs_ibfk_2` FOREIGN KEY (`artist_id`) REFERENCES `artist` (`id`) ON DELETE CASCADE,"
-    "  CONSTRAINT `songs_ibfk_3` FOREIGN KEY (`file_id`) REFERENCES `files` (`id`) ON DELETE CASCADE"
+    "  CONSTRAINT `songs_ibfk_1` FOREIGN KEY (`album_id`) "
+    "  REFERENCES `album` (`id`) ON DELETE CASCADE,"
+    "  CONSTRAINT `songs_ibfk_2` FOREIGN KEY (`artist_id`) "
+    "  REFERENCES `artist` (`id`) ON DELETE CASCADE,"
+    "  CONSTRAINT `songs_ibfk_3` FOREIGN KEY (`file_id`) "
+    "  REFERENCES `files` (`id`) ON DELETE CASCADE"
     ") ENGINE=InnoDB")
 
 
@@ -115,8 +120,10 @@ TABLES_CREATE['playlist'] = (
     "  PRIMARY KEY (`id`),"
     "  KEY `song_id` (`song_id`),"
     "  KEY `user_id` (`song_id`),"
-    "  CONSTRAINT `playlist_ibfk_1` FOREIGN KEY (`song_id`) REFERENCES `songs` (`id`) ON DELETE CASCADE,"
-    "  CONSTRAINT `playlist_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `login` (`id`) ON DELETE CASCADE"
+    "  CONSTRAINT `playlist_ibfk_1` FOREIGN KEY (`song_id`) "
+    "  REFERENCES `songs` (`id`) ON DELETE CASCADE,"
+    "  CONSTRAINT `playlist_ibfk_2` FOREIGN KEY (`user_id`) "
+    "  REFERENCES `login` (`id`) ON DELETE CASCADE"
     ") ENGINE=InnoDB")
 
 TABLES_CREATE['starred_songs'] = (
@@ -127,8 +134,10 @@ TABLES_CREATE['starred_songs'] = (
     "  PRIMARY KEY (`id`),"
     "  KEY `user_id` (`user_id`),"
     "  KEY `song_id` (`song_id`),"
-    "  CONSTRAINT `starred_songs_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `login` (`id`) ON DELETE CASCADE,"
-    "  CONSTRAINT `starred_songs_ibfk_2` FOREIGN KEY (`song_id`) REFERENCES `songs` (`id`) ON DELETE CASCADE"
+    "  CONSTRAINT `starred_songs_ibfk_1` FOREIGN KEY (`user_id`) "
+    "  REFERENCES `login` (`id`) ON DELETE CASCADE,"
+    "  CONSTRAINT `starred_songs_ibfk_2` FOREIGN KEY (`song_id`) "
+    "  REFERENCES `songs` (`id`) ON DELETE CASCADE"
     ") ENGINE=InnoDB")
 
 TABLES_CREATE['starred_albums'] = (
@@ -139,8 +148,10 @@ TABLES_CREATE['starred_albums'] = (
     "  PRIMARY KEY (`id`),"
     "  KEY `user_id` (`user_id`),"
     "  KEY `album_id` (`album_id`),"
-    "  CONSTRAINT `starred_albums_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `login` (`id`) ON DELETE CASCADE,"
-    "  CONSTRAINT `starred_albums_ibfk_2` FOREIGN KEY (`album_id`) REFERENCES `album` (`id`) ON DELETE CASCADE"
+    "  CONSTRAINT `starred_albums_ibfk_1` FOREIGN KEY (`user_id`) "
+    "  REFERENCES `login` (`id`) ON DELETE CASCADE,"
+    "  CONSTRAINT `starred_albums_ibfk_2` FOREIGN KEY (`album_id`) "
+    "  REFERENCES `album` (`id`) ON DELETE CASCADE"
     ") ENGINE=InnoDB")
 
 TABLES_CREATE['statistics'] = (
@@ -150,7 +161,8 @@ TABLES_CREATE['statistics'] = (
     "  `played` int unsigned NOT NULL,"
     "  PRIMARY KEY (`id`),"
     "  KEY `song_id` (`song_id`),"
-    "  CONSTRAINT `statistics_ibfk_1` FOREIGN KEY (`song_id`) REFERENCES `songs` (`id`) ON DELETE CASCADE"
+    "  CONSTRAINT `statistics_ibfk_1` FOREIGN KEY (`song_id`) "
+    "  REFERENCES `songs` (`id`) ON DELETE CASCADE"
     ") ENGINE=InnoDB")
 
 TABLES_DROP = ["status", "status_types", "configuration",
@@ -169,11 +181,11 @@ QUERIES["status_types"] = """ INSERT INTO status_types
 
 VIEWS_CREATE["albums_info_by_artist_album"] = (
     "CREATE ALGORITHM=UNDEFINED SQL SECURITY DEFINER VIEW `albums_info_by_artist_album`"
-    " AS select `ar`.`name` AS `artist_name`,`a`.`name` AS `album_name`,(sum(`s`.`duration`) / 60) AS `duration`,count(`s`.`title`) AS `tracks`, `a`.`image` AS `album_image`"
+    " AS select `ar`.`name` AS `artist_name`,`a`.`name` AS `album_name`, "
+    " (sum(`s`.`duration`) / 60) AS `duration`,count(`s`.`title`) AS `tracks`, "
+    " `a`.`image` AS `album_image`"
     " from ((`songs` `s` join `album` `a` on((`a`.`id` = `s`.`album_id`)))"
     " join `artist` `ar` on((`ar`.`id` = `s`.`artist_id`)))"
     " group by `ar`.`name`,`a`.`name`, `a`.`image` order by `ar`.`name`,`a`.`name`")
 
-VIEWS_DROP = ["albums_info_by_artist_album"] 
-
-# CREATE ALGORITHM=UNDEFINED SQL SECURITY DEFINER VIEW `albums_info_by_artist_album` AS select `ar`.`name` AS `artist_name`,`a`.`name` AS `album_name`,(sum(`s`.`duration`) / 60) AS `duration`,count(`s`.`title`) AS `tracks` from ((`songs` `s` join `album` `a` on((`a`.`id` = `s`.`album_id`))) join `artist` `ar` on((`ar`.`id` = `s`.`artist_id`))) group by `ar`.`name`,`a`.`name` order by `ar`.`name`,`a`.`name`;
+VIEWS_DROP = ["albums_info_by_artist_album"]
